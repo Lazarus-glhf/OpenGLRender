b84d6fd5cc14e38608446bc079d83650e2942197
Lazarus-glhf <redrumlivedog@outlook.com>
2023-02-20 11:40:09 +0800

    Just a save

--- Application.cpp
+++ Application.cpp
@@ -1,1 +1,39 @@
+#include "GL/glew.h"
 #include "GLFW/glfw3.h"
+#include "glm/glm.hpp"
+
+#include <iostream>
+
+int main(void)
+{
+	GLFWwindow* window;
+
+	if (!glfwInit())
+	{
+		return -1;
+	}
+
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "GLEW init failed!" << std::endl;
+		return -2;
+	}
+
+	while(!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		
+	}
+}
+
