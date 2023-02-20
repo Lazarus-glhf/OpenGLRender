0ee10d8e33112ae21fc0935826b61779a3b16479
Lazarus-glhf <redrumlivedog@outlook.com>
2023-02-20 14:40:40 +0800

    everything you need to draw the firsrt triangle (I guess

--- Application.cpp
+++ Application.cpp
@@ -17,7 +17,6 @@ int main(void)
 	if (!window)
 	{
 		glfwTerminate();
-
 		return -1;
 	}
 
@@ -29,11 +28,35 @@ int main(void)
 		return -2;
 	}
 
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
+	float positions[6] = {
+		-0.5f, -0.5f,
+		 0.0f,  0.5f,
+		 0.5f, -0.5f
+	};
+
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (const void*)0);
+
+
+	/* Loop until user close the windows */
 	while(!glfwWindowShouldClose(window))
 	{
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		
+		glfwSwapBuffers(window);
+		glfwPollEvents();
 	}
+
+	glfwTerminate();
+	return 0;
 }
 
