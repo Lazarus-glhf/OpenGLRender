b95de3de33c8a05654ad48f0083a04862109f70b
Lazarus-glhf <redrumlivedog@outlook.com>
2023-02-22 10:01:07 +0800

    abstract Shader

---
+++ src/Shader.cpp
@@ -0,0 +1,131 @@
+#include "Shader.h"
+#include "Renderer.h"
+
+#include <iostream>
+#include <GL/glew.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+Shader::Shader(const std::string& filePath)
+	:m_FilePath(filePath), m_RendererID(0)
+{
+	ShaderProgramSource source = ParseShader(filePath);
+	m_RendererID = CreateShader(source.VertexSource, source.FragmentSource);
+}
+
+Shader::~Shader()
+{
+	GLCall(glDeleteProgram(m_RendererID));
+}
+
+void Shader::Bind() const
+{
+	GLCall(glUseProgram(m_RendererID));
+}
+
+void Shader::Unbind() const
+{
+	GLCall(glUseProgram(0));
+}
+
+unsigned int Shader::CompileShader(unsigned int type, const std::string& source)
+{
+	unsigned int id = glCreateShader(type);
+	const char* src = source.c_str();
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE)
+	{
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		char* message = (char*)alloca(length * sizeof(char));
+		glGetShaderInfoLog(id, length, &length, message);
+
+		std::cout << "Failed to compile" << (type == GL_VERTEX_SHADER ? "vertexShader" : "fragmentShader") << "shader" << std::endl;
+		std::cout << message << std::endl;
+
+		glDeleteShader(id);
+
+		return 0;
+	}
+
+	return id;
+	return false;
+}
+
+unsigned int Shader::CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
+}
+
+ShaderProgramSource Shader::ParseShader(const std::string& filePath)
+{
+	std::ifstream stream(filePath);
+
+	enum class Shadertype
+	{
+		NONE = -1, VERTEX = 0, FRAGEMENT = 1
+	};
+
+	std::string line;
+	std::stringstream ss[2];
+	Shadertype type = Shadertype::NONE;
+	while (getline(stream, line))
+	{
+		if (line.find("#shader") != std::string::npos)
+		{
+			if (line.find("vertex") != std::string::npos)
+			{
+				type = Shadertype::VERTEX;
+			}
+			else if (line.find("fragment") != std::string::npos)
+			{
+				type = Shadertype::FRAGEMENT;
+			}
+		}
+		else
+		{
+			ss[int(type)] << line << '\n';
+		}
+	}
+
+	return { ss[0].str(), ss[1].str() };
+}
+
+void Shader::SetUniform4f(const std::string name, float v0, float v1, float v2, float v3)
+{
+	GLCall(glUniform4f(GetUniformLocation(name), v0, v1, v2, v3));
+}
+
+int Shader::GetUniformLocation(const std::string& name)
+{
+	if (m_UniformLocationCache.find(name) != m_UniformLocationCache.end())
+	{
+		return m_UniformLocationCache[name];
+	}
+	GLCall(unsigned int location = glGetUniformLocation(m_RendererID, name.c_str()));
+	if (location == -1)
+	{
+		std::cout << "Warning: uniform " << name << " doesn't exist!" << std::endl;
+	}
+	m_UniformLocationCache[name] = location;
+
+	return location;
+}
--- src/VertexArray.cpp
+++ src/VertexArray.cpp
@@ -27,7 +27,8 @@ void VertexArray::AddBuffer(const VertexBuffer& vb, const VertexBufferLayout& la
 		std::cout << "count: " << element.count << " " << "Stride: " << layout.GetStride() << std::endl;
 		GLCall(glVertexAttribPointer(i, element.count, element.type, 
 			element.normalized, layout.GetStride(), (const void*)offset));
-		offset = element.count * VertexBufferElement::GetSizeOfType(element.type);
+		// TODO offset? += : =
+		offset += element.count * VertexBufferElement::GetSizeOfType(element.type);
 	}
 }
 
