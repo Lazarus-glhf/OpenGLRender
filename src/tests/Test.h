12700fe7dd2b4064ef8dbe2723d253d175e440dc
Lazarus-glhf <redrumlivedog@outlook.com>
2023-02-23 17:26:19 +0800

    fix bug

--- src/tests/Test.h
+++ src/tests/Test.h
@@ -30,7 +30,7 @@ namespace test
 		{
 			std::cout << "Registering test " << name << std::endl;
 
-			m_Tests.push_back(std::make_pair(name, []() { return dynamic_cast<Test*> (new T)(); }));
+			m_Tests.push_back(std::make_pair(name, []() { return dynamic_cast<Test*> (new T()); }));
 		}
 
 	private:
