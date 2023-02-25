90c4977d8680287497b283b77309983fed2d9e00
Lazarus-glhf <redrumlivedog@outlook.com>
2023-02-26 03:03:41 +0800

    make uniform faster

--- src/Shader.cpp
+++ src/Shader.cpp
@@ -109,27 +109,49 @@ ShaderProgramSource Shader::ParseShader(const std::string& filePath)
 	return { ss[0].str(), ss[1].str() };
 }
 
-void Shader::SetUniform1i(const std::string name, int value)
+void Shader::SetUniform1i(const std::string name, int value) const
 {
-	GLCall(glUniform1i(GetUniformLocation(name), value));
+	GLint location = GetUniformLocation(name);
+	GLCall(glUniform1i(location, value));
 }
 
-void Shader::SetUniform1f(const std::string name, float value)
+void Shader::SetUniform1f(const std::string name, float value) const
 {
-	GLCall(glUniform1f(GetUniformLocation(name), value));
+	GLint location = GetUniformLocation(name);
+	GLCall(glUniform1i(location, value));
 }
 
-void Shader::SetUniform4f(const std::string name, float v0, float v1, float v2, float v3)
+void Shader::SetUniform2f(const std::string name, glm::vec2 value) const
 {
-	GLCall(glUniform4f(GetUniformLocation(name), v0, v1, v2, v3));
+	GLint location = GetUniformLocation(name);
+	GLCall(glUniform2f(location, value.x, value.y));
 }
 
-void Shader::SetUniformMat4f(const std::string name, const glm::mat4& matrix)
+void Shader::SetUniform3f(const std::string name, glm::vec3 value) const
 {
-	GLCall(glUniformMatrix4fv(GetUniformLocation(name), 1, GL_FALSE, &matrix[0][0]));
+	GLint location = GetUniformLocation(name);
+	GLCall(glUniform3f(location, value.x, value.y, value.z));
 }
 
-int Shader::GetUniformLocation(const std::string& name)
+void Shader::SetUniform4f(const std::string name, glm::vec4 value) const
+{
+	GLint location = GetUniformLocation(name);
+	GLCall(glUniform4f(location, value.x, value.y, value.z, value.w));
+}
+ 
+void Shader::SetUniformMat3f(const std::string name, const glm::mat3& matrix) const
+{
+	GLint location = GetUniformLocation(name);
+	GLCall(glUniformMatrix4fv(location, 1, GL_FALSE, &matrix[0][0]));
+}
+
+void Shader::SetUniformMat4f(const std::string name, const glm::mat4& matrix) const
+{
+	GLint location = GetUniformLocation(name);
+	GLCall(glUniformMatrix4fv(location, 1, GL_FALSE, &matrix[0][0]));
+}
+
+int Shader::GetUniformLocation(const std::string& name) const
 {
 	if (m_UniformLocationCache.find(name) != m_UniformLocationCache.end())
 	{
--- src/Shader.h
+++ src/Shader.h
@@ -16,7 +16,7 @@ class Shader
 private:
 	std::string m_FilePath;
 	unsigned int m_RendererID;
-	std::unordered_map<std::string, int> m_UniformLocationCache;
+	mutable std::unordered_map<std::string, int> m_UniformLocationCache;
 
 public:
 	Shader(const std::string& filePath);
@@ -26,10 +26,14 @@ public:
 	void Unbind() const;
 
 	// Set uniforms
-	void SetUniform1i(const std::string name, int value);
-	void SetUniform1f(const std::string name, float value);
-	void SetUniform4f(const std::string name, float v0, float v1, float v2, float v3);
-	void SetUniformMat4f(const std::string name, const glm::mat4& matrix);
+	void SetUniform1i(const std::string name, int value) const;
+	void SetUniform1f(const std::string name, float value) const;
+	void SetUniform2f(const std::string name, glm::vec2 value) const;
+	void SetUniform3f(const std::string name, glm::vec3 value) const;
+	void SetUniform4f(const std::string name, glm::vec4 value) const;
+
+	void SetUniformMat3f(const std::string name, const glm::mat3& matrix) const;
+	void SetUniformMat4f(const std::string name, const glm::mat4& matrix) const;
 
 private:
 	unsigned int CompileShader(unsigned int type, const std::string& source);
@@ -38,5 +42,5 @@ private:
 
 	ShaderProgramSource ParseShader(const std::string& filePath);
 
-	int GetUniformLocation(const std::string& name);
+	int GetUniformLocation(const std::string& name) const;
 };
